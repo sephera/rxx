9135aa3bd6a27f985b921d9a3dd370cac0e118d0
Hai Nguyen <nduyhai@users.noreply.github.com>
2020-02-21 10:24:34 +0700

    update

---
+++ src/main/java/com/example/rxx/RxxApplication.java
@@ -0,0 +1,13 @@
+package com.example.rxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rxx/RxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
